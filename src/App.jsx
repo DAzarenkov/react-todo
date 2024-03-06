db395af264dad49e6cfdfd900475a6ed6c4212f2
Dmytro Azarenkov <azarenkov.mintymint@gmail.com>
2024-03-07 00:44:31 +0200

    Add assignment for lesson 1_2

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
-import './App.css'
-
-const todoList = [
-  { id: 1, title: "Complete assignment" },
-  { id: 2, title: "Buy groceries" },
-  { id: 3, title: "Go to the gym" },
-  { id: 4, title: "Read a book" },
-  { id: 5, title: "Plan weekend activities" }
-];
+import './App.css';
+import TodoList from "./TodoList.jsx";
+import AddTodoForm from "./AddTodoForm.jsx";
 
 function App() {
 
   return (
     <>
       <h1>Todo List</h1>
-      <ul>
-        {todoList.map(function (listItem) {
-          return <li key={listItem.id}>{listItem.title}</li>
-        })}
-      </ul>
+      <AddTodoForm />
+      <TodoList/>
     </>
   )
 }
