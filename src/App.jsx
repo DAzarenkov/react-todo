2ce1f6a2beb3f2ab6983a01ee6291b6244b78a1f
Dmytro Azarenkov <azarenkov.mintymint@gmail.com>
2024-05-05 23:24:13 +0300

    Add a POST feature to add todos to the Airtable API

--- src/App.jsx
+++ src/App.jsx
@@ -42,6 +42,50 @@ function App() {
     }
   };
 
+  const storeData = async (data) => {
+
+    let body = {
+      records: [
+        {
+          fields: {
+            title: data.title
+          }
+        }
+      ]
+    };
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    };
+
+    const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+
+    try {
+      const response = await fetch(url, options);
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+
+      let data = await response.json();
+      let newTodo = data.records.map((todo) => {
+        return {
+          title: todo.fields.title,
+          id: todo.id
+        };
+      });
+
+      setTodoList([...todoList, ...newTodo]);
+    } catch (error) {
+      console.log(error.message)
+    }
+  };
+
   useEffect(() => {
     fetchData();
   }, []);
@@ -53,7 +97,9 @@ function App() {
   }, [todoList]);
 
   const addTodo = (newTodo) => {
-    setTodoList([...todoList, newTodo]);
+    //setTodoList([...todoList, newTodo]);
+
+    storeData(newTodo);
   };
 
   const removeTodo = (id) => {
